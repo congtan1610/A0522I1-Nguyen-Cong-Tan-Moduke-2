691462d60f684d1b794d9d64f501b2ec5dab4331
congtan1610 <congtan125@gmail.com>
2022-08-29 20:22:16 +0700

    adjhg

--- src/ss08_Refactoring/bt/TennisGame.java
+++ src/ss08_Refactoring/bt/TennisGame.java
@@ -1,11 +1,10 @@
 package ss08_Refactoring.bt;
 
 public class TennisGame {
-    public static String getScore(String player1Name, String player2Name, int m_score1, int m_score2) {
+    public static String getScore(String playerName1, String playerName2, int pointForPlayer1, int pointForPlayer2) {
         String score = "";
-        int tempScore = 0;
-        if (m_score1 == m_score2) {
-            switch (m_score1) {
+        if (pointForPlayer1 == pointForPlayer2) {
+            switch (pointForPlayer1) {
                 case 0:
                     score = "Love-All";
                     break;
@@ -22,35 +21,47 @@ public class TennisGame {
                     score = "Deuce";
                     break;
             }
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
-            int minusResult = m_score1 - m_score2;
-            if (minusResult == 1) score = "Advantage player1";
-            else if (minusResult == -1) score = "Advantage player2";
-            else if (minusResult >= 2) score = "Win for player1";
-            else score = "Win for player2";
+        } else if (pointForPlayer1 >= 4 || pointForPlayer2 >= 4) {
+            score = getFinalResult(pointForPlayer1, pointForPlayer2);
         } else {
-            for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = m_score1;
-                else {
-                    score += "-";
-                    tempScore = m_score2;
-                }
-                switch (tempScore) {
-                    case 0:
-                        score += "Love";
-                        break;
-                    case 1:
-                        score += "Fifteen";
-                        break;
-                    case 2:
-                        score += "Thirty";
-                        break;
-                    case 3:
-                        score += "Forty";
-                        break;
-                }
+            score = plusPoint(pointForPlayer1, pointForPlayer2, score);
+        }
+        return score;
+    }
+
+    private static String plusPoint(int pointForPlayer1, int pointForPlayer2, String score) {
+        int tempScore;
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) tempScore = pointForPlayer1;
+            else {
+                score += "-";
+                tempScore = pointForPlayer2;
+            }
+            switch (tempScore) {
+                case 0:
+                    score += "Love";
+                    break;
+                case 1:
+                    score += "Fifteen";
+                    break;
+                case 2:
+                    score += "Thirty";
+                    break;
+                case 3:
+                    score += "Forty";
+                    break;
             }
         }
+        return score;
+    }
+
+    private static String getFinalResult(int pointForPlayer1, int pointForPlayer2) {
+        String score;
+        int minusResult = pointForPlayer1 - pointForPlayer2;
+        if (minusResult == 1) score = "Advantage player1";
+        else if (minusResult == -1) score = "Advantage player2";
+        else if (minusResult >= 2) score = "Win for player1";
+        else score = "Win for player2";
         return score;
     }
 }
